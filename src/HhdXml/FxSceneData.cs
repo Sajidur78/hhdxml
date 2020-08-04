88f66ba164c46f7d1fd60585abd80eb19ffa9a1d
Sajid <sajidur78@gmail.com>
2020-08-04 16:57:17 +0600

    Initial Commit

---
+++ src/HhdXml/FxSceneData.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HhdXml
+{
+    public enum GIModeTypes : byte
+    {
+        GIMODE_NORMAL = 0,
+        GIMODE_ONLY = 1,
+        GIMODE_NONE = 2,
+        GIMODE_SHADOW = 3,
+        GIMODE_SEPARATED = 4,
+    };
+
+    public enum LightFieldModeTypes : byte
+    {
+        LFMODE_NORMAL = 0,
+        LFMODE_ONLY = 1,
+        LFMODE_NONE = 2,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxSceneConfig
+    {
+        public float Gammatvwiiu;
+        public float Gammadrcwiiu;
+        public bool Fixedldr;
+        public GIModeTypes Gimode;
+        public LightFieldModeTypes Lightfieldmode;
+        public bool Drawlightfieldsamplingpoints;
+        public bool Updatelightfieldeachframe;
+        public bool Drawlightfieldregion;
+        public int Screenshotlargescale;
+        public bool Drawfxcolgeom;
+        public bool Drawfxcolname;
+        public bool Drawlocallightsphere;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxCullingSettings
+    {
+        public float Rangedefault;
+        public float Rangenear;
+        public float Rangemiddle;
+        public float Rangefar;
+    }
+
+    public enum PeepingPlayerType : byte
+    {
+        TYPE_DEFAULT = 0,
+        TYPE_EDGE = 1,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxSceneSettings
+    {
+        public float Skyintensityscale;
+        public float Skyfollowupratioy;
+        public bool Pseudofogenable;
+        public bool Pseudofogwithoutfar;
+        public bool Pseudodof;
+        public bool Deepblurenable;
+        public bool Noblurenable;
+        public float Blurscale;
+        public bool Peepingplayerenable;
+        public float Occcheckedplayertime;
+        public PeepingPlayerType Peepingplayertype;
+        public bool Clearfirstsurface;
+        public bool Usemanualzprepass;
+        public bool Usecaptureframebuffercolor;
+        public bool Usecaptureframebufferdepth;
+        public bool Playerdrawoverlay;
+    }
+
+    public enum EyeLightMode : byte
+    {
+        LIGHTMODE_DIR = 0,
+        LIGHTMODE_POINT = 1,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxLightSettings
+    {
+        public bool Globallightenable;
+        public bool Amblightenable;
+        public bool Alllocallightenable;
+        public bool Eyelightenable;
+        public EyeLightMode Eyelightmode;
+        public Vector3 Eyelightdiffuse;
+        public Vector3 Eyelightspecular;
+        public float Eyelightrangestart;
+        public float Eyelightrangeend;
+    }
+
+    public enum SaturationScalingType : byte
+    {
+        SATURATION_KEEPING_LUMINANCE = 0,
+        SATURATION_KEEPING_BRIGHTNESS = 1,
+        SATURATION_NONE = 2,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxLightFieldSettings
+    {
+        public bool Ignoredata;
+        public byte Defaultupdateinterval;
+        public Vector3 Offsetcolorup;
+        public Vector3 Offsetcolordown;
+        public SaturationScalingType Saturationscalingtype;
+        public float Saturationscalingrate;
+        public float Luminancescalingrate;
+        public bool Disablefinaladjustcolor;
+        public float Luminancemin;
+        public float Luminancemax;
+        public float Luminancemidium;
+        public float Intensitythreshold;
+        public float Intensitybias;
+        public float Defaultinterruption;
+        public Vector3 Defaultcolorup;
+        public Vector3 Defaultcolordown;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxLightScatteringSettings
+    {
+        public bool Enable;
+        public Vector3 Color;
+        public float Depthscale;
+        public float Inscatteringscale;
+        public float Rayleigh;
+        public float Mie;
+        public float G;
+        public float Znear;
+        public float Zfar;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxHdrSettings
+    {
+        public bool Enable;
+        public bool Adaptationenable;
+        public float Adaptationratio;
+        public float Middlegray;
+        public float Luminancelow;
+        public float Luminancehigh;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxGlareSettings
+    {
+        public bool Enable;
+        public float Brightpassthreshold;
+        public float Brightpassinvscale;
+        public float Persistent;
+        public float Bloomscale;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxDofSettings
+    {
+        public bool Enable;
+        public bool Ignoresky;
+        public float Focus;
+        public float Znear;
+        public float Zfar;
+        public float Focusrange;
+    }
+
+    public enum TimeType : byte
+    {
+        TIME_NONE = 0,
+        TIME_MORNING = 1,
+        TIME_DAY = 2,
+        TIME_EVENING = 3,
+        TIME_NIGHT = 4,
+    };
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxHourSettings
+    {
+        public float Middlegray;
+        public Vector3 Basecolor;
+        public Vector3 Light;
+        public float Skyintensity;
+        public Vector3 Sky;
+        public Vector3 Ambient;
+        public Vector3 Lightscattering;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxTimeChangeSettings
+    {
+        public bool Enable;
+        public bool Ignoresky;
+        public byte Timedebugindex;
+        public float Morning;
+        public float Day;
+        public float Evening;
+        public float Night;
+
+        public FxHourSettings HourParam1;
+        public FxHourSettings HourParam2;
+        public FxHourSettings HourParam3;
+        public FxHourSettings HourParam4;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxInShadowShadowScaleSettings
+    {
+        public float Shadowscalex;
+        public float Shadowscaley;
+        public float Shadowscalelightfieldy;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxOnSceneShadowScaleSettings
+    {
+        public float Shadowscalez;
+        public float Shadowscalew;
+        public float Shadowscalelightfieldw;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxShadowScale
+    {
+        public FxInShadowShadowScaleSettings Shadowscaleinshadow;
+        public FxOnSceneShadowScaleSettings Shadowscaleonscene;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxGrassSettings
+    {
+        public bool Grassishide;
+        public float Grassheightmin;
+        public float Grassheight;
+        public float Grasswidth;
+        public float Grassfar;
+        public float Grassfarend;
+        public float Grasswindaxis;
+        public float Grasswindspeed;
+        public float Grasswindcycle;
+        public float Grasswindstrength;
+        public uint Grassdupcount;
+        public float Grassduprange;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxStageDistortion
+    {
+        public bool Distortionisuse;
+        public float Distortionspeed;
+        public float Distortionpower;
+        public float Distortiondensity;
+        public float Distortiondepthdensity;
+        public float Distortionpowerbloom;
+        public float Distortionpowerdepth;
+        public float Distortionpowerfront;
+        public float Distortiondensityfront;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxStencilShadow
+    {
+        public bool Enable;
+        public Vector3 Shadowcolor;
+        public float Shadowalpha;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxEffectSettings
+    {
+        public float Lightscale;
+        public float Shadowscale;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxSceneCasinoLight
+    {
+        public bool Isusecasinolight;
+        public Vector3 Casinolightaabbmin;
+        public Vector3 Casinolightaabbmax;
+        public float Casinolightmoveratio;
+        public float Casinolightstrengthmax;
+        public float Casinolightradmin;
+        public float Casinolightradmax;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxParameter
+    {
+        public FxCullingSettings Culling;
+        public FxSceneSettings Scene;
+        public FxLightSettings Light;
+        public FxLightFieldSettings Lightfield;
+        public FxLightScatteringSettings Olsnear;
+        public bool Separateolslayer;
+        public FxLightScatteringSettings Olsfar;
+        public FxHdrSettings Hdr;
+        public FxGlareSettings Glare;
+        public FxDofSettings Dof;
+        public FxTimeChangeSettings Timechange;
+        public FxShadowScale Shadowscale;
+        public FxGrassSettings Grasssetting;
+        public FxStageDistortion Stagedistortion;
+        public FxStencilShadow Stencilshadow;
+        public FxEffectSettings Effect;
+        public FxSceneCasinoLight Casinolight;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct FxSceneData
+    {
+        public FxSceneConfig Config;
+
+        public FxParameter Item1;
+        public FxParameter Item2;
+        public FxParameter Item3;
+        public FxParameter Item4;
+    }
+}
---
+++ src/HhdXml/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HhdXml
+{
+    class Program
+    {
+        static unsafe void Main(string[] cliArgs)
+        {
+            var inputFile = string.Empty;
+            var inputXml = string.Empty;
+            string outputFile;
+
+            var args = ParseArguments(cliArgs);
+
+            if (args.Count == 1 && string.IsNullOrEmpty(args[string.Empty].FirstOrDefault()))
+            {
+                ShowHelp();
+                return;
+            }
+
+            if (args.TryGetValue("help", out _))
+            {
+                ShowHelp();
+                return;
+            }
+
+            if (args.TryGetValue("hhd", out var hhdValues))
+            {
+                var name = hhdValues.FirstOrDefault();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Invalid input file.");
+                    return;
+                }
+
+                try
+                {
+                    if (!File.Exists(name))
+                    {
+                        Console.WriteLine("File not found!");
+                        return;
+                    }
+
+                    inputFile = name;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid filename.");
+                    return;
+                }
+            }
+            else
+            {
+                var name = args[string.Empty].FirstOrDefault();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Invalid input file.");
+                    return;
+                }
+
+                try
+                {
+                    if (!File.Exists(name))
+                    {
+                        Console.WriteLine("File not found!");
+                        return;
+                    }
+
+                    if (name.EndsWith(".hhd", StringComparison.OrdinalIgnoreCase))
+                        inputFile = name;
+                    else if (name.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
+                        inputXml = name;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid filename.");
+                    return;
+                }
+            }
+
+            if (args.TryGetValue("xml", out var xmlValues))
+            {
+                var name = xmlValues.FirstOrDefault();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Invalid input file.");
+                    return;
+                }
+
+                try
+                {
+                    if (!File.Exists(name))
+                    {
+                        Console.WriteLine("File not found!");
+                        return;
+                    }
+
+                    inputXml = name;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid filename.");
+                    return;
+                }
+            }
+
+            if (args.TryGetValue("output_file", out var outputValues))
+            {
+                var name = outputValues.FirstOrDefault();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Invalid input file.");
+                    return;
+                }
+
+                try
+                {
+                    outputFile = Path.GetFullPath(name);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid filename.");
+                    return;
+                }
+            }
+            else
+            {
+                outputFile = Path.ChangeExtension(string.IsNullOrEmpty(inputFile) ? inputXml : inputFile, string.IsNullOrEmpty(inputXml) ? ".xml" : ".hhd");
+            }
+
+            if (string.IsNullOrEmpty(inputXml))
+            {
+                var buffer = File.ReadAllBytes(inputFile);
+
+                fixed (byte* buf = buffer)
+                {
+                    var sceneData = (FxSceneData*)buf;
+                    var serializer = new XmlSerializer(typeof(FxSceneData));
+                    serializer.Serialize(File.Create(outputFile), *sceneData);
+                }
+            }
+            else
+            {
+                var serializer = new XmlSerializer(typeof(FxSceneData));
+                using (var stream = new FileStream(inputXml, FileMode.Open, FileAccess.Read))
+                {
+                    var sceneData = (FxSceneData)serializer.Deserialize(stream);
+                    var buffer = new byte[sizeof(FxSceneData)];
+                    fixed (byte* bufferPtr = buffer)
+                        *(FxSceneData*)bufferPtr = sceneData;
+
+                    File.WriteAllBytes(outputFile, buffer);
+                }
+            }
+        }
+
+        static Dictionary<string, List<string>> ParseArguments(string[] args, string prefix = "--")
+        {
+            var result = new Dictionary<string, List<string>>();
+            List<string> selectedArgs = new List<string>();
+            result[string.Empty] = selectedArgs;
+
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith(prefix))
+                {
+                    selectedArgs = new List<string>();
+                    result[arg.Substring(prefix.Length)] = selectedArgs;
+                    continue;
+                }
+
+                selectedArgs.Add(arg);
+            }
+
+            return result;
+        }
+
+        static void ShowHelp()
+        {
+            Console.Write(@"hhdxml: A tool for converting Sonic Lost World (PC) hhd files to xml and back.
+Arguments:
+    --help (Show this help message)
+    --hhd input_file (Input hhd file to use)
+    --xml input_xml (Input xml file to use, if converting back to hhd)
+    --output_file output_file (Path to output file)
+
+Examples:
+    hhdxml.exe --hhd w1a01.hhd --output_file w1a01.xml
+    hhdxml.exe --xml w1a01.xml --output_file w1a01.hhd
+    hhdxml.exe w1a01.hhd
+
+");
+        }
+    }
+}
